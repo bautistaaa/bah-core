a8ea4bd19d656bdd17631e4f74dc49d90f6541ea
bautistaaa <chrisbautistaaa@gmail.com>
2025-04-21 20:41:07 -0700

    chore: add ui lib with secondary entry point

--- apps/bah-core/src/app/app.component.ts
+++ apps/bah-core/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ButtonComponent } from '@bah-core/ui/button';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
---
+++ libs/ui/button/README.md
@@ -0,0 +1,3 @@
+# @bah-core/ui/button
+
+Secondary entry point of `@bah-core/ui`. It can be used by importing from `@bah-core/ui/button`.
