1720bba5f940789a213c326fa4e325ae718fecaf
bautistaaa <chrisbautistaaa@gmail.com>
2025-04-30 23:20:05 -0700

    feat: add copy button to code component

--- apps/trash-bin/src/app/pages/components/code/code.component.ts
+++ apps/trash-bin/src/app/pages/components/code/code.component.ts
@@ -8,111 +8,109 @@ import 'highlight.js/styles/github-dark.css';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="code-container bg-gray-50 dark:bg-gray-800 rounded-lg p-4 my-4">
-      <pre class="m-0"><code [class]="language" [innerHTML]="highlightedCode"></code></pre>
+    <div
+      class="bg-gray-700 dark:bg-gray-800 rounded-lg p-4 my-4 relative group"
+    >
+      <button
+        (click)="copyToClipboard()"
+        class="absolute top-2 right-2 p-1.5 text-gray-400 hover:text-gray-200 transition-colors duration-200 border border-gray-600 hover:border-gray-400 rounded cursor-pointer"
+      >
+        <svg
+          *ngIf="!copied"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
+          <path
+            d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
+          />
+        </svg>
+        <svg
+          *ngIf="copied"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+      <pre
+        class="m-0 text-gray-200"
+      ><code [class]="language" [innerHTML]="highlightedCode"></code></pre>
     </div>
   `,
-  styles: [`
-    :host ::ng-deep {
-      .hljs {
-        background: transparent;
-        color: #24292e;
+  styles: [
+    `
+      :host ::ng-deep {
+        .hljs {
+          background: transparent;
+          color: #e6edf3;
+        }
+
+        .hljs-keyword,
+        .hljs-selector-tag,
+        .hljs-subst {
+          color: #ff7b72;
+        }
+
+        .hljs-string,
+        .hljs-doctag {
+          color: #a5d6ff;
+        }
+
+        .hljs-title,
+        .hljs-section,
+        .hljs-selector-id {
+          color: #d2a8ff;
+        }
+
+        .hljs-attr,
+        .hljs-attribute {
+          color: #79c0ff;
+        }
+
+        .hljs-symbol,
+        .hljs-bullet,
+        .hljs-built_in,
+        .hljs-addition,
+        .hljs-variable,
+        .hljs-template-variable {
+          color: #7ee787;
+        }
+
+        .hljs-comment,
+        .hljs-quote,
+        .hljs-deletion {
+          color: #8b949e;
+        }
       }
-      
-      .dark .hljs {
+
+      pre {
+        white-space: pre-wrap;
+      }
+
+      code {
+        font-family: 'SFMono-Regular', Consolas, 'Liberation Mono', Menlo,
+          Courier, monospace;
+        font-size: 14px;
+        line-height: 1.5;
         color: #e6edf3;
       }
-      
-      .hljs-keyword,
-      .hljs-selector-tag,
-      .hljs-subst {
-        color: #d73a49;
-      }
-      
-      .dark .hljs-keyword,
-      .dark .hljs-selector-tag,
-      .dark .hljs-subst {
-        color: #ff7b72;
-      }
-      
-      .hljs-string,
-      .hljs-doctag {
-        color: #032f62;
-      }
-      
-      .dark .hljs-string,
-      .dark .hljs-doctag {
-        color: #a5d6ff;
-      }
-      
-      .hljs-title,
-      .hljs-section,
-      .hljs-selector-id {
-        color: #6f42c1;
-      }
-      
-      .dark .hljs-title,
-      .dark .hljs-section,
-      .dark .hljs-selector-id {
-        color: #d2a8ff;
-      }
-      
-      .hljs-attr,
-      .hljs-attribute {
-        color: #005cc5;
-      }
-      
-      .dark .hljs-attr,
-      .dark .hljs-attribute {
-        color: #79c0ff;
-      }
-      
-      .hljs-symbol,
-      .hljs-bullet,
-      .hljs-built_in,
-      .hljs-addition,
-      .hljs-variable,
-      .hljs-template-variable {
-        color: #22863a;
-      }
-      
-      .dark .hljs-symbol,
-      .dark .hljs-bullet,
-      .dark .hljs-built_in,
-      .dark .hljs-addition,
-      .dark .hljs-variable,
-      .dark .hljs-template-variable {
-        color: #7ee787;
-      }
-      
-      .hljs-comment,
-      .hljs-quote,
-      .hljs-deletion {
-        color: #6a737d;
-      }
-      
-      .dark .hljs-comment,
-      .dark .hljs-quote,
-      .dark .hljs-deletion {
-        color: #8b949e;
-      }
-    }
-    
-    pre {
-      white-space: pre-wrap;
-    }
-    
-    code {
-      font-family: 'SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace;
-      font-size: 14px;
-      line-height: 1.5;
-    }
-  `]
+    `,
+  ],
 })
 export class CodeComponent implements OnInit {
   @Input() code = '';
   @Input() language = 'typescript';
   highlightedCode = '';
+  copied = false;
 
   ngOnInit() {
     this.highlightCode();
@@ -120,10 +118,24 @@ export class CodeComponent implements OnInit {
 
   private highlightCode() {
     try {
-      this.highlightedCode = hljs.highlight(this.code, { language: this.language }).value;
+      this.highlightedCode = hljs.highlight(this.code, {
+        language: this.language,
+      }).value;
     } catch (error) {
       console.error('Error highlighting code:', error);
       this.highlightedCode = this.code;
     }
   }
-} 
+
+  async copyToClipboard() {
+    try {
+      await navigator.clipboard.writeText(this.code);
+      this.copied = true;
+      setTimeout(() => {
+        this.copied = false;
+      }, 2000);
+    } catch (error) {
+      console.error('Failed to copy text:', error);
+    }
+  }
+}
